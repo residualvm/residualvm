sev-/scummvm-new

CREATE_PROJECT: Disable edit and continue in the scummvm project
Edit and continue is not compatible with the coroutine code. Previously,
it was disabled in the tinsel project only, but now that the coroutine
code has been moved into common, we need to disable edit and continue
in the scummvm project instead

devtools/create_project/visualstudio.cpp
@@ -110,7 +110,7 @@ void VisualStudioProvider::createProjectFile(const std::string &name, const std:
 
 		std::string toolConfig;
 		toolConfig  = (!warnings.empty() ? "DisableSpecificWarnings=\"" + warnings + "\"" : "");
-		toolConfig += (name == "tinsel" ? "DebugInformationFormat=\"3\" " : "");
+		toolConfig += (name == "scummvm" ? "DebugInformationFormat=\"3\" " : "");
 		toolConfig += (name == "sword25" ? "DisableLanguageExtensions=\"false\" " : "");
 		toolConfig += (name == "grim" ? "DisableLanguageExtensions=\"false\" " : "");
 

residualvm/residualvm

devtools/create_project/visualstudio.cpp
@@ -110,7 +110,7 @@ void VisualStudioProvider::createProjectFile(const std::string &name, const std:
 
 		std::string toolConfig;
 		toolConfig  = (!warnings.empty() ? "DisableSpecificWarnings=\"" + warnings + "\"" : "");
-		toolConfig += (name == "tinsel" ? "DebugInformationFormat=\"3\" " : "");
+		toolConfig += (name == "residualvm" ? "DebugInformationFormat=\"3\" " : "");
 		toolConfig += (name == "sword25" ? "DisableLanguageExtensions=\"false\" " : "");
 		toolConfig += (name == "grim" ? "DisableLanguageExtensions=\"false\" " : "");
 		toolConfig += (name == "myst3" ? "DisableLanguageExtensions=\"false\" " : "");