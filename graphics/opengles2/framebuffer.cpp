38abbc799a6f1f2cda6b19abdc6e016095ee17cc
Dries Harnie <dries@harnie.be>
2013-04-27 11:12:48 +0200

    MISC: Remove GLCALL debugging from framebuffer.cpp

--- graphics/opengles2/framebuffer.cpp
+++ graphics/opengles2/framebuffer.cpp
@@ -24,87 +24,44 @@
 
 #include "graphics/opengles2/framebuffer.h"
 
-#ifdef ANDROID_DEBUG_GL
-#include <android/log.h>
-
-extern const char *android_log_tag;
-
-#define _ANDROID_LOG(prio, fmt, args...) __android_log_print(prio, android_log_tag, fmt, ## args)
-#define LOGD(fmt, args...) _ANDROID_LOG(ANDROID_LOG_DEBUG, fmt, ##args)
-#define LOGI(fmt, args...) _ANDROID_LOG(ANDROID_LOG_INFO, fmt, ##args)
-#define LOGW(fmt, args...) _ANDROID_LOG(ANDROID_LOG_WARN, fmt, ##args)
-#define LOGE(fmt, args...) _ANDROID_LOG(ANDROID_LOG_ERROR, fmt, ##args)
-
-extern void checkGlError(const char *expr, const char *file, int line);
-
-#ifdef ANDROID_DEBUG_GL_CALLS
-#define GLCALLLOG(x, before) \
-	do { \
-		if (before) \
-			LOGD("calling '%s' (%s:%d)", x, __FILE__, __LINE__); \
-		else \
-			LOGD("returned from '%s' (%s:%d)", x, __FILE__, __LINE__); \
-	} while (false)
-#else
-#define GLCALLLOG(x, before) do {  } while (false)
-#endif
-
-#define GLCALL(x) \
-	do { \
-		GLCALLLOG(#x, true); \
-		(x); \
-		GLCALLLOG(#x, false); \
-		checkGlError(#x, __FILE__, __LINE__); \
-	} while (false)
-
-#define GLTHREADCHECK \
-	do { \
-		assert(pthread_self() == _main_thread); \
-	} while (false)
-
-#else
-#define GLCALL(x) do { (x); } while (false)
-#define GLTHREADCHECK do {  } while (false)
-#endif
-
 namespace Graphics {
 
 FrameBuffer::FrameBuffer(GLuint texture_name, uint width, uint height) : _colorTexture(texture_name), _width(width), _height(height) {
-GLCALL(	glGenFramebuffers(1, &_frameBuffer));
-GLCALL(	glGenRenderbuffers(1, &_depthRenderBuffer));
+	glGenFramebuffers(1, &_frameBuffer);
+	glGenRenderbuffers(1, &_depthRenderBuffer);
 
-GLCALL(	glBindRenderbuffer(GL_RENDERBUFFER, _depthRenderBuffer));
-GLCALL(	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height));
-GLCALL(	glBindRenderbuffer(GL_RENDERBUFFER, 0));
+	glBindRenderbuffer(GL_RENDERBUFFER, _depthRenderBuffer);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height);
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
-GLCALL(	glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer));
-GLCALL(	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture_name, 0));
-GLCALL(	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _depthRenderBuffer));
+	glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture_name, 0);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _depthRenderBuffer);
 
-GLCALL(	glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer));
+	glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer);
 	GLenum status=glCheckFramebufferStatus(GL_FRAMEBUFFER);
 	if (status != GL_FRAMEBUFFER_COMPLETE)
 		error("Framebuffer is not complete! status: %d", status);
 
-GLCALL(	glBindTexture(GL_TEXTURE_2D, 0));
-GLCALL(	glBindFramebuffer(GL_FRAMEBUFFER, 0));
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 FrameBuffer::~FrameBuffer() {
-GLCALL(	glDeleteRenderbuffers(1, &_depthRenderBuffer));
-GLCALL(	glDeleteFramebuffers(1, &_frameBuffer));
+	glDeleteRenderbuffers(1, &_depthRenderBuffer);
+	glDeleteFramebuffers(1, &_frameBuffer);
 }
 
 void FrameBuffer::attach(uint actual_width, uint actual_height) {
-GLCALL(	glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer));
-GLCALL(	glViewport(0,0, actual_width, actual_height));
+	glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer);
+	glViewport(0,0, actual_width, actual_height);
 
-GLCALL(	glClearColor(0, 0, 0, 1.0f));
-GLCALL(	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+	glClearColor(0, 0, 0, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void FrameBuffer::detach() {
-GLCALL(	glBindFramebuffer(GL_FRAMEBUFFER, 0));
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 }
