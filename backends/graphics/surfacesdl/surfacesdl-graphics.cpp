dbe7e3c12a8a64845afc24b2a6d1301a83644e41
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2014-07-20 07:42:49 +0200

    SDL2: disabled GL forward compatible mode, it seems fail create context on 3.0 GL driver, probably because not setting version of context

--- backends/graphics/surfacesdl/surfacesdl-graphics.cpp
+++ backends/graphics/surfacesdl/surfacesdl-graphics.cpp
@@ -252,7 +252,10 @@ Graphics::PixelBuffer SurfaceSdlGraphicsManager::setupScreen(uint screenW, uint 
 		//SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 		// make GL forward compatible
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+		// FIXME: disabled for now, it seems not work for only GL >= 3.0,
+		// it fails create context, probably because not setting above major/minor version
+		// of GL context
+		//SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
 #endif
 		_glContext = SDL_GL_CreateContext(_window);
 		if (!_glContext) {
