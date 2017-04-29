baaac7db1ade3ca7a3d63f281fa03efb6ebcb756
Vincent Pelletier <plr.vincent@gmail.com>
2017-04-29 10:10:44 +0000

    MYST3: Use TGL_UNSIGNED_SHORT_5_6_5 texture format in gfx_tinygl
    It is now supported by TinyGL.

--- engines/myst3/gfx_tinygl_texture.cpp
+++ engines/myst3/gfx_tinygl_texture.cpp
@@ -35,7 +35,7 @@ TinyGLTexture::TinyGLTexture(const Graphics::Surface *surface) {
 		sourceFormat = TGL_UNSIGNED_INT_8_8_8_8_REV;
 	} else if (format.bytesPerPixel == 2) {
 		internalFormat = TGL_RGB;
-		sourceFormat = TGL_UNSIGNED_BYTE; // UNSIGNED_SHORT_5_6_5 not provided
+		sourceFormat = TGL_UNSIGNED_SHORT_5_6_5;
 	} else
 		error("Unknown pixel format");
 
