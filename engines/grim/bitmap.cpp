ae0cc090442d2fc96ad7034d49305618a1a12ddb
Vincent Pelletier <plr.vincent@gmail.com>
2017-05-03 08:07:08 +0000

    EMI: Fix Bitmap loading on big-endian

--- engines/grim/bitmap.cpp
+++ engines/grim/bitmap.cpp
@@ -335,13 +335,12 @@ bool BitmapData::loadTile(Common::SeekableReadStream *o) {
 				uint8 r = (p & 0x001F) << 3;
 				uint8 a = (p & 0x8000) ? 0xFF : 0x00;
 				// Recombine the color components into a 32 bit RGB value
-				uint32 tmp = (r << 24) | (g << 16) | (b << 8) | a;
-				WRITE_BE_UINT32(&d[j], tmp);
+				d[j] = (a << 24) | (b << 16) | (g << 8) | r;
 			}
 		} else if (_bpp == 32) {
 			uint32 *d = (uint32 *)data[i];
 			for (int j = 0; j < _width * _height; ++j) {
-				o->read(&(d[j]), 4);
+				d[j] = o->readUint32LE();
 			}
 		}
 	}
